e4944455239a08391d3b2da1edda3631dd5c8900
Ritik180 <54978100+Ritik180@users.noreply.github.com>
2023-04-07 13:49:34 +0530

    Update square.py

--- square/square.py
+++ square/square.py
@@ -1,11 +1,3 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/square')
-def square():
-    number = request.args.get('number', type=int)
-    return str(number ** 2)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+# square.py
+def square(num):
+    return num ** 2
