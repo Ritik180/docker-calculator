2b5774348791e14873d44c83d233630417064c4a
Ritik180 <54978100+Ritik180@users.noreply.github.com>
2023-04-07 13:50:03 +0530

    Update cube.py

--- cube/cube.py
+++ cube/cube.py
@@ -1,11 +1,3 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/cube')
-def cube():
-    number = request.args.get('number', type=int)
-    return str(number ** 3)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+# cube.py
+def cube(num):
+    return num ** 3
